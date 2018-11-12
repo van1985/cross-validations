3c7c1f5f4240113a1ee0ca837ea6c613c4753107
leandro.garcia <leandro.garcia@globant.com>
2018-11-12 17:05:34 -0300

    Initial Structure

---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as formatAmount } from './utils/mb-amount-format-util/mb-amount-format-util';
---
+++ utils/mb-amount-format-util/mb-amount-format-util.js
@@ -0,0 +1,41 @@
+// @flow
+
+const privateFormatAmount = (
+    number,
+    decimalLength = 2,
+    sectionDelimiter = '.',
+    decimalLimiter = ','
+) => {
+    let parsedNumber = parseFloat(number);
+    if (parsedNumber != number && number) {
+        parsedNumber = parseFloat(number.replace(decimalLimiter, sectionDelimiter));
+    }
+    if (!isNaN(parsedNumber)) {
+        const re = '\\d(?=(\\d{' + 3 + '})+' + (decimalLength > 0 ? '\\D' : '$') + ')';
+        const num = parsedNumber.toFixed(Math.max(0, ~~decimalLength));
+
+        return num
+            .replace('.', decimalLimiter)
+            .replace(new RegExp(re, 'g'), '$&' + (sectionDelimiter || ','));
+    } else {
+        return '';
+    }
+};
+
+const formatAmount = (number, lang, decimals = 2, showCurrency = true) => {
+    switch (lang) {
+        case 'en':
+            return privateFormatAmount(number, decimals, ',', '.');
+        case 'ar':
+            return privateFormatAmount(number, decimals, '.', ',') + (showCurrency ? " ARS" : "");
+        case 'ch':
+            return privateFormatAmount(number, decimals, '.', ',') + (showCurrency ? " CLP" : "");
+        case 'es':
+        default:
+            return privateFormatAmount(number, decimals, '.', ',') + (showCurrency ? " €" : "");
+    }
+};
+
+module.exports = {
+    formatAmount
+};
